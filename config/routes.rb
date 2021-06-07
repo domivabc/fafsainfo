f943697d8da3a03f85ccbee79701357580e85c7b
vagner domingues <domivabc@gmail.com>
2021-06-07 03:03:07 -0400

    rails-admin installed; Events controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do  
 
+  resources :events
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root 'home#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_060919) do
+ActiveRecord::Schema.define(version: 2021_06_07_063232) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
